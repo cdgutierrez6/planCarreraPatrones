3c0afb54df6b2e4f1fff73b61e73337faff1df68
SATRACK\Daniel_Gutierrez <Daniel_Gutierrez@satrack.com>
2023-08-04 08:20:48 -0500

    Se modifica la implementación del singleton

--- Patrones/Creational/Singleton/Logger.cs
+++ Patrones/Creational/Singleton/Logger.cs
@@ -4,8 +4,10 @@ using System.IO;
 // Clase Singleton Logger para registrar mensajes en un archivo de registro
 public sealed class Logger
 {
-    private static readonly Logger instance = new Logger();
+    private static Logger instance;
+    private static readonly object lockObject = new object();
     private string logFilePath;
+
 
     // Constructor privado para evitar la creación de instancias desde fuera de la clase
     private Logger()
@@ -14,9 +16,21 @@ public sealed class Logger
     }
 
     // Propiedad estática para acceder a la única instancia
-    public static Logger Instance
+    // Método estático para obtener la instancia única
+    public static Logger GetInstance()
     {
-        get { return instance; }
+        if (instance == null)
+        {
+            lock (lockObject)
+            {
+                if (instance == null)
+                {
+                    instance = new Logger();
+                }
+            }
+        }
+
+        return instance;
     }
 
     // Método para registrar un mensaje en el archivo de registro
